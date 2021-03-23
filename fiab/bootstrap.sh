001f6b08d3e07882b563c92f0e65fc7dd9d4a5ec
Myungjin Lee (myungjle) <myungjle@cisco.com>
2021-03-23 11:02:53 -0700

    controller and worker nodes (#6)
    one controller and two worker nodes are configured.

--- fiab/bootstrap.sh
+++ fiab/bootstrap.sh
@@ -1,35 +1,21 @@
 #! /usr/bin/env bash
 
-function setup() {
-    sudo apt update
-    sudo apt upgrade -y
-
-    ERLANG_GPG_KEY=https://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc
-    wget -O- $ERLANG_GPG_KEY | sudo apt-key add -
-    echo "deb https://packages.erlang-solutions.com/ubuntu focal contrib" | \
-	sudo tee /etc/apt/sources.list.d/rabbitmq.list
-
-    sudo apt update
-
-    sudo apt install -y build-essential erlang libsnappy-dev libssl-dev net-tools
+function controller {
+    sudo systemctl enable vernemq
+    sudo systemctl start vernemq
 }
 
-function install_vernemq() {
-    cd /
-    VERNEMQ=vernemq
-    VERNEMQ_VERSION=1.11.0
-    git clone https://github.com/$VERNEMQ/$VERNEMQ.git
-
-    cd $VERNEMQ
-    git checkout tags/$VERNEMQ_VERSION
-    make rel
+function worker {
+    # do something
+    cat /dev/null
 }
 
-function main() {
-    # no need to call setup function if myungjin-lee/fledge box is in use
-    # setup
-
-    install_vernemq
+function main {
+    if [ `hostname` == "controller" ]; then
+	controller
+    else
+	worker
+    fi
 }
 
 main
