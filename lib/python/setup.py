48faef73bc3f171a945e40cb7a537c1e9a9968ce
Myungjin Lee (myungjle) <myungjle@cisco.com>
2021-06-28 08:50:46 -0700

    create fledge python package (#30)
    We turn fledge python library into a package. For that, a couple of
    files (setup.py, setup.cfg, LICENSE.txt, CHANGES.txt, etc.) are added.

---
+++ lib/python/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fledge',
+    version='0.0.1',
+    author='An Awesome Coder',
+    author_email='myungjle@cisco.com',
+    packages=find_packages(),
+    scripts=[],
+    url='https://wwwin-github.cisco.com/fledge/fledge/',
+    license='LICENSE.txt',
+    description=
+    'This package is a python library to run ML workloads in the fledge system',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'cloudpickle', 'keras', 'numpy', 'scikit-learn', 'tensorflow', 'torch'
+    ]
+)
