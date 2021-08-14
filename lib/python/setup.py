a1067eeb85e9758424b3ea6df694b4880f35e5e4
Myungjin Lee (myungjle) <myungjle@cisco.com>
2021-08-14 14:52:44 -0700

    install additional files in python setup.py (#79)
    To facilitate demo in k8s env, configuration files of examples are
    included in the package. These example configuration files are not
    essential to the package. Hence, these can be removed in the future.

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -5,7 +5,48 @@ setup(
     version='0.0.1',
     author='Myungjin Lee',
     author_email='myungjle@cisco.com',
+    include_package_data=True,
     packages=find_packages(),
+    # TODO: remove data_files later as it is not essential
+    data_files=[
+        (
+            'fledge/examples/hier_mnist/gaggr', [
+                'fledge/examples/hier_mnist/gaggr/config.json'
+            ]
+        ),
+        (
+            'fledge/examples/hier_mnist/aggregator', [
+                'fledge/examples/hier_mnist/aggregator/config_uk.json',
+                'fledge/examples/hier_mnist/aggregator/config_us.json'
+            ]
+        ),
+        (
+            'fledge/examples/hier_mnist/trainer', [
+                'fledge/examples/hier_mnist/trainer/config_uk.json',
+                'fledge/examples/hier_mnist/trainer/config_us.json'
+            ]
+        ),
+        (
+            'fledge/examples/mnist/aggregator', [
+                'fledge/examples/mnist/aggregator/config.json'
+            ]
+        ),
+        (
+            'fledge/examples/mnist/trainer', [
+                'fledge/examples/mnist/trainer/config.json'
+            ]
+        ),
+        (
+            'fledge/examples/simple/bar', [
+                'fledge/examples/simple/bar/config.json'
+            ]
+        ),
+        (
+            'fledge/examples/simple/foo', [
+                'fledge/examples/simple/foo/config.json'
+            ]
+        )
+    ],
     scripts=[],
     url='https://wwwin-github.cisco.com/eti/fledge/',
     license='LICENSE.txt',
