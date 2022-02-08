6bd1f47d4c7c5f6eefea1a0f6d20af1ba47935b9
Myungjin Lee <myungjin@users.noreply.github.com>
2022-02-08 13:33:02 -0800

    deployment: multi-image build/push support (#51)
    We support different images for control plane and data plane because
    each plane's requirement is slightly differnt.

--- build/build-docker.sh
+++ build/build-docker.sh
@@ -7,41 +7,6 @@ BASE_DOCKER_IMAGE=${PROJECT}:base
 H_OUT=index.html
 S_OUT=staticanalysis.txt
 
-get_artifactory_credentials() {
-  if [[ "${ARTIFACTORY_USER}" != "" ]] && [[ "${ARTIFACTORY_PASSWORD}" != "" ]]; then
-    return
-  fi
-
-  if [[ "${NYOTA_CREDENTIALS_FILE}" = "" ]]; then
-    NYOTA_CREDENTIALS_FILE=~/.nyota/credentials
-    echo "Using DEFAULT Artifactory credentials file: $NYOTA_CREDENTIALS_FILE"
-  else
-    echo "Using Artifactory credentials file: $NYOTA_CREDENTIALS_FILE"
-  fi
-
-  [[ "$(uname)" = Darwin ]] && NYOTA_CREDENTIALS_FILE_MOD=$(stat -f "%p" "${NYOTA_CREDENTIALS_FILE}" | cut -c4-) || NYOTA_CREDENTIALS_FILE_MOD=$(stat -c "%a" "${NYOTA_CREDENTIALS_FILE}")
-  if [[ ${NYOTA_CREDENTIALS_FILE_MOD} != "400" ]]; then
-    echo "File ${NYOTA_CREDENTIALS_FILE} must have 400 mod permission"
-    exit 1
-  fi
-
-  if [[ "$NYOTA_CREDENTIALS_SECTION" == "" ]]; then
-    NYOTA_CREDENTIALS_SECTION=default
-  fi
-
-  while IFS=' = ' read key value; do
-    if [[ ${key} == \[*] ]]; then
-      section=${key}
-    elif [[ ${value} ]] && [[ ${section} == "[${NYOTA_CREDENTIALS_SECTION}]" ]]; then
-      if [[ ${key} == 'artifactory_user' ]]; then
-        ARTIFACTORY_USER=${value}
-      elif [[ ${key} == 'artifactory_password' ]]; then
-        ARTIFACTORY_PASSWORD=${value}
-      fi
-    fi
-  done <${NYOTA_CREDENTIALS_FILE}
-}
-
 code_coverage() {
     # extract the H_OUT file from the docker image created
     id=$(docker create ${BASE_DOCKER_IMAGE})
@@ -90,15 +55,8 @@ do
     esac
 done
 
-# get_artifactory_credentials
 echo BUILDING DOCKER ${BASE_DOCKER_IMAGE}
 
-# export GO111MODULE=on
-# export GOPRIVATE="wwwin-github.cisco.com"
-# export GONOPROXY="github.com,gopkg.in,go.uber.org"
-# export GOPROXY=https://${ARTIFACTORY_USER}:${ARTIFACTORY_PASSWORD}@engci-maven-master.cisco.com/artifactory/api/go/nyota-go
-
-# docker pull containers.cisco.com/eti-sre/sre-golang-docker:latest
 docker build --no-cache \
     -t ${BASE_DOCKER_IMAGE} \
     -f build/Dockerfile \
@@ -119,23 +77,21 @@ docker build --no-cache \
 # fi
 
 echo BUILDING DOCKER ${DOCKER_IMAGE}
-docker build --no-cache -t ${DOCKER_IMAGE} -f build/Imagefile .
 
-# Create fledge worker images
-WORKER_IMAGE_NAME=flegde-worker
-# FRAMEWORKS=(allinone pytorch tensorflow)
-# RESOURCES=(cpu gpu)
-FRAMEWORKS=(allinone)
-RESOURCES=(cpu)
-for framework in ${FRAMEWORKS[@]}; do
-    for resource in ${RESOURCES[@]}; do
-        target=${framework}-${resource}
-        docker build \
-               -t ${WORKER_IMAGE_NAME}:${target} \
-               -f build/WorkerImagefile \
-               --build-arg TARGETIMAGE=${target} .
-    done
-done
+if [[ "${DOCKER_IMAGE}" == "fledge-worker"* ]]; then
+    # Create fledge worker image
+    target=${DOCKER_IMAGE}
+    target=${target#"fledge-worker-"}
+    target=${target%:*}
+    docker --no-cache build -t ${DOCKER_IMAGE} \
+           -f build/WorkerImagefile \
+           --build-arg TARGETIMAGE=${target} .
 
-# remove base image and its subordinate images
-# docker image rmi ${BASE_DOCKER_IMAGE} || true
+elif [[ "${DOCKER_IMAGE}" == "fledge:"* ]]; then
+    # Create fledge control plane image
+    docker build --no-cache -t ${DOCKER_IMAGE} -f build/Imagefile .
+
+else
+    echo "Error: Unknown type $DOCKER_IMAGE"
+    exit 1
+fi
