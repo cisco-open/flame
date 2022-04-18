a618e76c0a1765e88add54fc2de0fdeb6f4fc727
Gaoxiang Luo <luo00042@umn.edu>
2022-04-18 11:28:18 -0500

    feature: medmnist example (#100)
    Added MedMNIST example via PyTorch. Deleted deprecated code. Modified json files with new format. Added the default value of batch size to configuration.

--- lib/python/flame/examples/mednist/aggregator/main.py
+++ lib/python/flame/examples/mednist/aggregator/main.py
@@ -14,27 +14,79 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+"""MedMNIST aggregator for PyTorch."""
 
-from ....common.util import install_packages
+import logging
 
-install_packages(['monai', 'sklearn', 'tqdm'])
+from flame.config import Config
+from flame.dataset import Dataset # Not sure why we need this.
+from flame.mode.horizontal.top_aggregator import TopAggregator
+import torch
 
-# example cmd: python3 -m flame.examples.mednist.aggregator.main --rounds 3
-# run the above command in flame/lib/python folder
+logger = logging.getLogger(__name__)
+
+class CNN(torch.nn.Module):
+    """CNN Class"""
+
+    def __init__(self, num_classes):
+        """Initialize."""
+        super(CNN, self).__init__()
+        self.num_classes = num_classes
+        self.features = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 6, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(6),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+            torch.nn.Conv2d(6, 16, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.fc = torch.nn.Linear(16 * 7 * 7, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+class PyTorchMedMNistAggregator(TopAggregator):
+    """PyTorch MedMNist Aggregator"""
+
+    def __init__(self, config: Config) -> None:
+        self.config = config
+        self.model = None
+        self.dataset: Dataset = None # Not sure why we need this.
+
+    def initialize(self):
+        """Initialize."""
+        self.model = CNN(num_classes=9)
+
+    def load_data(self) -> None:
+        """Load a test dataset."""
+        # Implement this if loading data is needed in aggregator
+        pass
+
+    def train(self) -> None:
+        """Train a model."""
+        # Implement this if training is needed in aggregator
+        pass
+
+    def evaluate(self) -> None:
+        """Evaluate (test) a model."""
+        # Implement this if testing is needed in aggregator
+        pass
+
 if __name__ == "__main__":
     import argparse
 
-    from .role import Aggregator
-
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument(
-        '--rounds', type=int, default=1, help='number of training rounds'
-    )
+    parser.add_argument('config', nargs='?', default="./config.json")
 
     args = parser.parse_args()
 
-    aggregator = Aggregator(
-        'flame/examples/mednist/aggregator/config.json',
-        args.rounds,
-    )
-    aggregator.run()
+    config = Config(args.config)
+
+    a = PyTorchMedMNistAggregator(config)
+    a.compose()
+    a.run()
