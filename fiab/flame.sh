82c7d7cdf7ca0406ef25328976a254d0390eefb2
dhruvsgarg <dhruv.s.garg@hotmail.com>
2022-08-10 22:58:39 -0400

    Separated flame-control and flame-deployer
    This pr enables two separate bash scripts (flame.sh and deployer.sh) to be used by the end user. The flame.sh script deploys the flame-control with controller, apiserver, default-deployer, mongodb etc. The deployer.sh on the other hand only creates a new deployer pod that is aimed at registering a new compute cluster with an existing flame control plane. Two separate helm charts are utilized by flame-control and flame-deployer.

--- fiab/flame.sh
+++ fiab/flame.sh
@@ -18,7 +18,7 @@
 
 RELEASE_NAME=flame
 
-FILE_OF_INTEREST=helm-chart/values.yaml
+FILE_OF_INTEREST=helm-chart/control/values.yaml
 LINES_OF_INTEREST="27,34"
 
 SED_MAC_FIX=
@@ -27,36 +27,49 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 function init {
-    cd helm-chart
+    cd helm-chart/control
     helm dependency update
-    cd ..
+    cd ../..
+}
+
+function pre_start_stop_check {
+    # verify that deployer.sh script's pods are not running. If they are, throw an error.
+    curr_flame_pods=`kubectl get pods -o=name -n flame| grep flame-deployer | grep -v "flame-deployer-default-" | sed "s/^.\{4\}//"`
+    echo "checking for deployer.sh pods..."
+    if [ -z "$curr_flame_pods" ]; then
+        echo "deployer.sh script was not run, proceeding with flame.sh"
+        return 1
+    else
+        echo "deployer.sh script was run, cannot proceed with flame.sh"
+        return 0
+    fi
 }
 
 function start {
     # install the flame helm chart
-    helm install --create-namespace --namespace $RELEASE_NAME $RELEASE_NAME helm-chart/
+    helm install --create-namespace --namespace $RELEASE_NAME $RELEASE_NAME helm-chart/control/
 
     # Wait until mongodb is up
     ready=false
     MONGODB_PODS=(flame-mongodb-0 flame-mongodb-1)
     echo -n "checking mongodb readiness"
     while [ "$ready" == "false" ]; do
-	sleep 5
-	ready=true
-	for pod in ${MONGODB_PODS[@]}; do
-	    output=`kubectl get pod -n flame $pod \
-		    -o jsonpath='{.status.containerStatuses[0].ready}'`
-	    if [ "$output" != "true" ]; then
-		echo -n "."
-		ready=false
-		break
-	    fi
-	done
+        sleep 5
+        ready=true
+        for pod in ${MONGODB_PODS[@]}; do
+            output=`kubectl get pod -n flame $pod \
+                    -o jsonpath='{.status.containerStatuses[0].ready}'`
+            if [ "$output" != "true" ]; then
+                echo -n "."
+                ready=false
+                break
+            fi
+        done
     done
     echo "done"
 
     if [ "$1" == "false" ]; then
-	return
+        return
     fi
 
     # Warning: The remaining part in this function is not stable.
@@ -68,7 +81,7 @@ function start {
     # uncomment the lines of interest and upgrade the release
     sed -i $SED_MAC_FIX -e "$LINES_OF_INTEREST"" s/^  # /  /" $FILE_OF_INTEREST
     
-    helm upgrade --namespace $RELEASE_NAME $RELEASE_NAME helm-chart/
+    helm upgrade --namespace $RELEASE_NAME $RELEASE_NAME helm-chart/control/
 
     # comment the lines of interest
     sed -i $SED_MAC_FIX -e "$LINES_OF_INTEREST"" s/^  /  # /" $FILE_OF_INTEREST
@@ -77,7 +90,7 @@ function start {
     # with '' (SED_MAX_FIX string). couldn't figure out why.
     # as a workaround, delete the backup file
     if [[ $SED_MAC_FIX ]]; then
-	rm -f $FILE_OF_INTEREST$SED_MAC_FIX
+        rm -f $FILE_OF_INTEREST$SED_MAC_FIX
     fi
 
     echo "done"
@@ -87,21 +100,21 @@ function post_start_config {
     minikube_ip=$(minikube ip)
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	subnet=$(ip a show | grep br- | grep inet | awk '{print $2}')
-	resolver_file=/etc/systemd/network/minikube.network
-	echo "[Match]" | sudo tee $resolver_file > /dev/null
-	echo "Name=br*" | sudo tee -a $resolver_file > /dev/null
-	echo "[Network]" | sudo tee -a $resolver_file > /dev/null
-	echo "Address=$subnet" | sudo tee -a $resolver_file > /dev/null
-	echo "DNS=$minikube_ip" | sudo tee -a $resolver_file > /dev/null
-	echo "Domains=~flame.test" | sudo tee -a $resolver_file > /dev/null
-	sudo systemctl restart systemd-networkd
+        subnet=$(ip a show | grep br- | grep inet | awk '{print $2}')
+        resolver_file=/etc/systemd/network/minikube.network
+        echo "[Match]" | sudo tee $resolver_file > /dev/null
+        echo "Name=br*" | sudo tee -a $resolver_file > /dev/null
+        echo "[Network]" | sudo tee -a $resolver_file > /dev/null
+        echo "Address=$subnet" | sudo tee -a $resolver_file > /dev/null
+        echo "DNS=$minikube_ip" | sudo tee -a $resolver_file > /dev/null
+        echo "Domains=~flame.test" | sudo tee -a $resolver_file > /dev/null
+        sudo systemctl restart systemd-networkd
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-	resolver_file=/etc/resolver/flame-test
-	echo "domain flame.test" | sudo tee $resolver_file > /dev/null
-	echo "nameserver $minikube_ip" | sudo tee -a $resolver_file > /dev/null
-	echo "search_order 1" | sudo tee -a $resolver_file > /dev/null
-	echo "timeout 5" | sudo tee -a $resolver_file > /dev/null
+        resolver_file=/etc/resolver/flame-test
+        echo "domain flame.test" | sudo tee $resolver_file > /dev/null
+        echo "nameserver $minikube_ip" | sudo tee -a $resolver_file > /dev/null
+        echo "search_order 1" | sudo tee -a $resolver_file > /dev/null
+        echo "timeout 5" | sudo tee -a $resolver_file > /dev/null
     fi
 
     # add dns entry for flame.test domain  in coredns
@@ -123,9 +136,9 @@ function post_start_config {
 
     # step 5: patch configmap of coredns with the updated dns entries
     kubectl patch configmap coredns \
-	    -n kube-system \
-	    --type merge \
-	    -p "$(cat $tmp_file)"
+        -n kube-system \
+        --type merge \
+        -p "$(cat $tmp_file)"
 
     rm -f $tmp_file $tmp_file$SED_MAC_FIX
 }
@@ -137,12 +150,12 @@ function stop {
 
     terminated=false
     while [ "$terminated" == "false" ]; do
-	sleep 5
-	echo -n "."
-	output=`kubectl get pods -n flame -o jsonpath='{.items[*].status.phase}'`
-	if [ -z "$output" ]; then
-	    terminated=true
-	fi
+        sleep 5
+        echo -n "."
+        output=`kubectl get pods -n flame -o jsonpath='{.items[*].status.phase}'`
+        if [ -z "$output" ]; then
+            terminated=true
+        fi
     done
 
     echo "done"
@@ -154,12 +167,12 @@ function post_stop_cleanup {
     minikube_ip=$(minikube ip)
 
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	resolver_file=/etc/systemd/network/minikube.network
-	sudo rm -f $resolver_file
-	sudo systemctl restart systemd-networkd
+        resolver_file=/etc/systemd/network/minikube.network
+        sudo rm -f $resolver_file
+        sudo systemctl restart systemd-networkd
     elif [[ "$OSTYPE" == "darwin"* ]]; then
-	resolver_file=/etc/resolver/flame-test
-	sudo rm -f $resolver_file
+        resolver_file=/etc/resolver/flame-test
+        sudo rm -f $resolver_file
     fi
 
     # remove dns entry for flame.test domain in coredns
@@ -171,23 +184,22 @@ function post_stop_cleanup {
 
     # patch coredns configmap only if flame.test domain section exists
     if [[ "$(grep flame.test $tmp_file)" != "" ]]; then 
-	# remove last five lines
-	for i in {1..5}; do
-	    sed -i $SED_MAC_FIX '$d' $tmp_file
-	done
+        # remove last five lines
+        for i in {1..5}; do
+            sed -i $SED_MAC_FIX '$d' $tmp_file
+        done
 
-	# step 4: create patch file
-	echo "{\"data\": {\"Corefile\": $(jq -R -s < $tmp_file)}}" > $tmp_file
+        # step 4: create patch file
+        echo "{\"data\": {\"Corefile\": $(jq -R -s < $tmp_file)}}" > $tmp_file
 
-	# step 5: patch configmap of coredns with the updated dns entries
-	kubectl patch configmap coredns \
-		-n kube-system \
-		--type merge \
-		-p "$(cat $tmp_file)"
+        # step 5: patch configmap of coredns with the updated dns entries
+        kubectl patch configmap coredns \
+            -n kube-system \
+            --type merge \
+            -p "$(cat $tmp_file)"
 
-	echo "removed flame.test domain section from coredns"
+        echo "removed flame.test domain section from coredns"
     fi
-
 
     rm -f $tmp_file $tmp_file$SED_MAC_FIX
 }
@@ -195,18 +207,30 @@ function post_stop_cleanup {
 function main {
     exposedb=false
     if [ "$2" == "--exposedb" ]; then
-	exposedb=true
+        exposedb=true
     fi
 
     if [ "$1" == "start" ]; then
-	init
-	start $exposedb
-	post_start_config
+        pre_start_stop_check
+        local start_stop_check_res=$?
+        if [ $start_stop_check_res == 1 ]; then
+            init
+            start $exposedb
+            post_start_config
+        else
+            echo "Exiting!"
+        fi
     elif [ "$1" == "stop" ]; then
-	stop
-	post_stop_cleanup
+        pre_start_stop_check
+        local start_stop_check_res=$?
+        if [ $start_stop_check_res == 1 ]; then
+            stop
+            post_stop_cleanup
+        else
+            echo "Exiting!"
+        fi
     else
-	echo "usage: ./flame.sh <start [--exposedb] | stop>"
+        echo "usage: ./flame.sh <start [--exposedb] | stop>"
     fi
 }
 
