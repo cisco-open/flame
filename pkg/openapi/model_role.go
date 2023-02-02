e76e7bd7d9ae82e5e28e59007c4e60d6a28d05b3
openwithcode <123649857+openwithcode@users.noreply.github.com>
2023-02-02 21:43:50 +0200

    Add group associations to roles (#319)

--- pkg/openapi/model_role.go
+++ pkg/openapi/model_role.go
@@ -34,4 +34,32 @@ type Role struct {
 	IsDataConsumer bool `json:"isDataConsumer,omitempty"`
 
 	Replica int32 `json:"replica,omitempty"`
+
+	GroupAssociation []map[string]string `json:"groupAssociation,omitempty"`
+}
+
+// AssertRoleRequired checks if the required fields are not zero-ed
+func AssertRoleRequired(obj Role) error {
+	elements := map[string]interface{}{
+		"name": obj.Name,
+	}
+	for name, el := range elements {
+		if isZero := IsZeroValue(el); isZero {
+			return &RequiredError{Field: name}
+		}
+	}
+
+	return nil
+}
+
+// AssertRecurseRoleRequired recursively checks if required fields are not zero-ed in a nested slice.
+// Accepts only nested slice of Role (e.g. [][]Role), otherwise ErrTypeAssertionError is thrown.
+func AssertRecurseRoleRequired(objSlice interface{}) error {
+	return AssertRecurseInterfaceRequired(objSlice, func(obj interface{}) error {
+		aRole, ok := obj.(Role)
+		if !ok {
+			return ErrTypeAssertionError
+		}
+		return AssertRoleRequired(aRole)
+	})
 }
