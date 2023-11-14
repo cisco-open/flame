f7490f772deac16287345c38a3e35570229b5e1b
raresgaia123 <137071040+raresgaia123@users.noreply.github.com>
2023-11-14 06:59:37 +0200

    Added dummy UI files and changed Dockerfile (#457)

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    console.log((window as unknown as any).env);
+  })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
